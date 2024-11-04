13d4e243abfadab0ee77a2ee81b01d0a7f39d23a
Ross Sharp <ross.sharp@dominodatalab.com>
2024-11-04 15:02:17 -0800

    Pushed from Domino: https://flows-devel.train-sandbox.domino.tech/workspace/ross_domino/CDISC01_RE_CSR?executionId=671a7a63c479da5d2daefe9f

--- flows/flow_1.py
+++ flows/flow_1.py
@@ -9,23 +9,17 @@ from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
 # Enter the command below to run this Flow. There is a single Flow input parameter for the SDTM Dataset snapshot
 # pyflyte run --remote flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND
 
-DataArtifact = Artifact("ADaM", DATA)
+DataArtifact = Artifact("ADaM Datasets", DATA)
 
 @workflow
-def ADaM_only(sdtm_dataset_snapshot: str) -> Tuple[
-    DataArtifact.File(name="adsl.sas7bdat"),
-    DataArtifact.File(name="adae.sas7bdat"),
-    DataArtifact.File(name="adcm.sas7bdat"),
-    DataArtifact.File(name="adlb.sas7bdat"),
-    DataArtifact.File(name="admh.sas7bdat"),
-    DataArtifact.File(name="advs.sas7bdat")]: 
+def ADaM_only(sdtm_dataset_snapshot: str):
 
     #Crete ADSL dataset. The only input is the SDTM Dataset. 
     adsl_task = run_domino_job_task(
         flyte_task_name="Create ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro"
     ) 
@@ -36,7 +30,7 @@ def ADaM_only(sdtm_dataset_snapshot: str) -> Tuple[
         command="prod/adam_flows/ADAE.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adae_dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
     )
@@ -46,7 +40,7 @@ def ADaM_only(sdtm_dataset_snapshot: str) -> Tuple[
         command="prod/adam_flows/ADCM.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adcm_dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
     )
@@ -56,7 +50,7 @@ def ADaM_only(sdtm_dataset_snapshot: str) -> Tuple[
         command="prod/adam_flows/ADLB.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adlb_dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
     )
@@ -66,7 +60,7 @@ def ADaM_only(sdtm_dataset_snapshot: str) -> Tuple[
         command="prod/adam_flows/ADMH.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="admh_dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
     )
@@ -76,11 +70,9 @@ def ADaM_only(sdtm_dataset_snapshot: str) -> Tuple[
         command="prod/adam_flows/ADVS.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="advs_dataset", type=FlyteFile[TypeVar("sas7bdat")])],
+        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
     )
 
-    # Output from the task above will be used in the next step
-
-    return adsl_task["adsl_dataset"], adae_task["adae_dataset"], adcm_task["adcm_dataset"], adlb_task["adlb_dataset"], admh_task["admh_dataset"], advs_task["advs_dataset"]
+    return
