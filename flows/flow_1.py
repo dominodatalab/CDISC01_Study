2041fcb0436d2e015deee3f3a8bf10543db9d063
Ross Sharp <ross.sharp@dominodatalab.com>
2024-11-05 15:08:59 -0800

    Pushed from Domino: https://flows-devel.train-sandbox.domino.tech/workspace/ross_domino/CDISC01_RE_CSR?executionId=671a7a63c479da5d2daefe9f

--- flows/flow_1.py
+++ flows/flow_1.py
@@ -5,11 +5,15 @@ from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
 
+sdtm_dataset="SDTMUNBLIND"
+sdtm_dataset_snapshot_number=1
 
 # Enter the command below to run this Flow. There is a single Flow input parameter for the SDTM Dataset snapshot
-# pyflyte run --remote flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND
+# pyflyte run --remote flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND 
+
 
 DataArtifact = Artifact("ADaM Datasets", DATA)
+
 
 @workflow
 def ADaM_only(sdtm_dataset_snapshot: str):
@@ -21,8 +25,9 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
         output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl.sas7bdat"))],
         use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
-    ) 
+        environment_name="SAS Analytics Pro",
+       # dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
+    )
 
     #Crete ADAE dataset. This has two inputs, the SDTM Dataset and the output from the previous task i.e. ADSL. 
     adae_task = run_domino_job_task(
@@ -33,6 +38,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
+        #dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
     )
     
     adcm_task = run_domino_job_task(
@@ -43,6 +49,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
+      #  dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
     )
 
     adlb_task = run_domino_job_task(
@@ -53,6 +60,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
+        #dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
     )
 
     admh_task = run_domino_job_task(
@@ -63,6 +71,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
+      #  dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
     )
 
     advs_task = run_domino_job_task(
@@ -73,6 +82,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs.sas7bdat"))],
         use_project_defaults_for_omitted=True,
         environment_name="SAS Analytics Pro",
+        #dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
     )
 
     return
