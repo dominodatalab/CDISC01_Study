0d098db8778c3f92e09454c85f1be1726496dc10
Ross Sharp <ross.sharp@dominodatalab.com>
2025-01-08 14:16:52 -0800

    t
    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/CDISC01_ReportingTeam/CDISC01_RE_CSR?executionId=677ee776f6e4350ab8cf0c40

--- utils/SDTM_transfer/ae.py
+++ utils/SDTM_transfer/ae.py
@@ -3,7 +3,7 @@ import shutil
 
 
 
-SDTM_DATA_PATH = os.environ["sdtm_dataset_snapshot"]
+SDTM_DATA_PATH = os.environ["sdtm_snapshot_task_input"]
 OUTPUT_PATH = "/workflow/outputs"
 
 src_file = os.path.join(SDTM_DATA_PATH, "ae.sas7bdat")
