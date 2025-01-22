28a5f16ac7d3af27a1378d32e28ed46733c581cc
Ross Sharp <ross.sharp@dominodatalab.com>
2025-01-22 13:20:10 -0800

    Updating flow_1
    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/CDISC01_ReportingTeam/CDISC01_RE_CSR?executionId=679153e3efde7d519a2cdcce

--- flows/flow_1.py
+++ flows/flow_1.py
@@ -13,6 +13,10 @@ hardware_tier_name="Small"
 # Enter the command below to run this Flow. There is a single Flow input parameter for the SDTM Dataset snapshot
 # pyflyte run --remote ./flows/flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND 
 
+# If you want to give the run a name, then use this command and replace the MY_CUSTOM_NAME argument
+# pyflyte run --remote --name MY_CUSTOM_NAME ./flows/flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND 
+
+
 # Define one Flow Artifact called ADaM Dataset to tag and group all of the ADAM task outputs as
 DataArtifact = Artifact("ADaM Datasets", DATA)
 
@@ -20,24 +24,22 @@ DataArtifact = Artifact("ADaM Datasets", DATA)
 @workflow
 def ADaM_only(sdtm_dataset_snapshot: str):
 
-    #Crete ADSL dataset. The only input is the SDTM Dataset. 
     adsl_task = run_domino_job_task(
         flyte_task_name="Create ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl.sas7bdat"))],
+        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
         use_project_defaults_for_omitted=True
     )
-
-    #Crete ADAE dataset. This has two inputs, the SDTM Dataset and the output from the previous task i.e. ADSL. 
+ 
     adae_task = run_domino_job_task(
         flyte_task_name="Create ADAE Dataset",
         command="prod/adam_flows/ADAE.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae.sas7bdat"))],
+        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
         use_project_defaults_for_omitted=True
@@ -48,7 +50,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         command="prod/adam_flows/ADCM.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm.sas7bdat"))],
+        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
         use_project_defaults_for_omitted=True
@@ -59,7 +61,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         command="prod/adam_flows/ADLB.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb.sas7bdat"))],
+        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
         use_project_defaults_for_omitted=True
@@ -70,7 +72,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         command="prod/adam_flows/ADMH.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh.sas7bdat"))],
+        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
         use_project_defaults_for_omitted=True
@@ -81,7 +83,7 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         command="prod/adam_flows/ADVS.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs.sas7bdat"))],
+        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
         use_project_defaults_for_omitted=True
