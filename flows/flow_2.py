3eaefcf65b07dd5e49462998102fdb01d7ff9ffd
Ross Sharp <ross.sharp@dominodatalab.com>
2025-01-22 13:31:20 -0800

    updating flow_2
    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/CDISC01_ReportingTeam/CDISC01_RE_CSR?executionId=679153e3efde7d519a2cdcce

--- flows/flow_2.py
+++ flows/flow_2.py
@@ -6,34 +6,44 @@ from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, 
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
 
 
-# Enter the command below to run this Flow. There are two Flow input parameters. One for the SDTM Dataset snapshot and one for the METADATA dataset snapshot.
-# pyflyte run --remote flow_2.py ADaM_TFL --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
+# Define variables to set the default compute environment and hardware tier for the Flow tasks
+environment_name="SAS Analytics Pro"
+hardware_tier_name="Small"
 
+
+# Enter the command below to run this Flow. There are two Flow input parameters. One for the SDTM Dataset snapshot and one for the METADATA dataset snapshot.
+# pyflyte run --remote ./flows/flow_2.py ADaM_TFL --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
+
+# If you want to give the run a name, then use this command and replace the MY_CUSTOM_NAME argument
+# pyflyte run --remote --name MY_CUSTOM_NAME ./flows/flow_2.py ADaM_TFL --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
+
+
+# Define two Flow Artifacts called ADaM Dataset and TFL Reports to tag and group ADaM and TFL outputs respectively
 DataArtifact = Artifact("ADaM Datasets", DATA)
 ReportArtifact = Artifact("TFL Reports", REPORT)
 
 @workflow
 def ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str): 
 
-   #Crete ADSL dataset. The only input is the SDTM Dataset. 
     adsl_task = run_domino_job_task(
         flyte_task_name="Create ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     ) 
 
-    #Crete ADAE dataset. This has two inputs, the SDTM Dataset and the output from the previous task i.e. ADSL. 
     adae_task = run_domino_job_task(
         flyte_task_name="Create ADAE Dataset",
         command="prod/adam_flows/ADAE.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     
     adcm_task = run_domino_job_task(
@@ -41,9 +51,10 @@ def ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADCM.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     adlb_task = run_domino_job_task(
@@ -51,9 +62,10 @@ def ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADLB.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     admh_task = run_domino_job_task(
@@ -61,9 +73,10 @@ def ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADMH.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     advs_task = run_domino_job_task(
@@ -71,9 +84,10 @@ def ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADVS.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     t_pop_task = run_domino_job_task(
@@ -81,9 +95,10 @@ def ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/tfl_flows/t_pop.sas",
         inputs=[Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="t_pop", type=ReportArtifact.File(name="t_pop.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="t_pop", type=ReportArtifact.File(name="t_pop", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     t_ae_rel_task = run_domino_job_task(
@@ -92,9 +107,21 @@ def ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         inputs=[Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"]),
                 Input(name="adae_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adae_task["adae_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="t_ae_rel", type=ReportArtifact.File(name="t_ae_rel.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="t_ae_rel", type=ReportArtifact.File(name="t_ae_rel", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
+    )
+
+    t_vscat_task = run_domino_job_task(
+        flyte_task_name="Create T_VSCAT Report",
+        command="prod/tfl_flows/t_vscat.sas",
+        inputs=[Input(name="advs_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=advs_task["advs_dataset"]),
+                Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
+        output_specs=[Output(name="t_vscat", type=ReportArtifact.File(name="t_vscat", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     return
