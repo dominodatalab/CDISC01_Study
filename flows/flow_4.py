70b2477add894bf28f9a3435a5328f4140afc14e
Ross Sharp <ross.sharp@dominodatalab.com>
2025-01-22 14:08:28 -0800

    updating flow_4
    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/CDISC01_ReportingTeam/CDISC01_RE_CSR?executionId=679153e3efde7d519a2cdcce

--- flows/flow_4.py
+++ flows/flow_4.py
@@ -6,9 +6,20 @@ from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, 
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
 
 
-# # Enter the command below to run this Flow. There are two Flow input parameters. One for the SDTM Dataset snapshot and one for the METADATA dataset snapshot.
-# pyflyte run --remote flow_4.py ADaM_TFL_QC --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
+# Define variables to set the default compute environment and hardware tier for the Flow tasks
+environment_name="SAS Analytics Pro"
+hardware_tier_name="Small"
 
+
+# Enter the command below to run this Flow. There are two Flow input parameters. One for the SDTM Dataset snapshot and one for the METADATA dataset snapshot.
+# pyflyte run --remote ./flows/flow_4.py ADaM_TFL_QC --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
+
+# If you want to give the run a name, then use this command and replace the MY_CUSTOM_NAME argument
+# pyflyte run --remote --name MY_CUSTOM_NAME ./flows/flow_4.py ADaM_TFL_QC --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA
+
+
+
+# Define four Flow Artifacts called ADaM Dataset, QC ADaM Dataset, TFL Reports and QC TFL Reports to tag and group ADaMs and TFLs respectively
 DataArtifact = Artifact("ADaM Datasets", DATA)
 QCDataArtifact = Artifact("QC ADaM Datasets", DATA)
 ReportArtifact = Artifact("TFL Reports", REPORT)
@@ -23,9 +34,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         flyte_task_name="Create ADSL Dataset",
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     ) 
 
     #PROD 
@@ -34,9 +46,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADAE.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #PROD 
     adcm_task = run_domino_job_task(
@@ -44,9 +57,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADCM.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #PROD 
     adlb_task = run_domino_job_task(
@@ -54,9 +68,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADLB.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb.sas7bdat"))],
+        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
         use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
     )
     #PROD 
     admh_task = run_domino_job_task(
@@ -64,9 +79,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADMH.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #PROD 
     advs_task = run_domino_job_task(
@@ -74,18 +90,20 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/adam_flows/ADVS.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #QC 
     qc_adsl_task = run_domino_job_task(
         flyte_task_name="Create QC ADSL Dataset",
         command="qc/adam_flows/qc_ADSL.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="qc_adsl_dataset", type=QCDataArtifact.File(name="qc_adsl.sas7bdat"))],
+        output_specs=[Output(name="qc_adsl_dataset", type=QCDataArtifact.File(name="qc_adsl", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
         use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
     ) 
  
     #QC 
@@ -94,9 +112,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="qc/adam_flows/qc_ADAE.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_adae_dataset", type=QCDataArtifact.File(name="qc_adae.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_adae_dataset", type=QCDataArtifact.File(name="qc_adae", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #QC 
     qc_adcm_task = run_domino_job_task(
@@ -104,9 +123,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="qc/adam_flows/qc_ADCM.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_adcm_dataset", type=QCDataArtifact.File(name="qc_adcm.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_adcm_dataset", type=QCDataArtifact.File(name="qc_adcm", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #QC 
     qc_adlb_task = run_domino_job_task(
@@ -114,9 +134,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="qc/adam_flows/qc_ADLB.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_adlb_dataset", type=QCDataArtifact.File(name="qc_adlb.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_adlb_dataset", type=QCDataArtifact.File(name="qc_adlb", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #QC 
     qc_admh_task = run_domino_job_task(
@@ -124,9 +145,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="qc/adam_flows/qc_ADMH.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_admh_dataset", type=QCDataArtifact.File(name="qc_admh.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_admh_dataset", type=QCDataArtifact.File(name="qc_admh", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     #QC 
     qc_advs_task = run_domino_job_task(
@@ -134,9 +156,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="qc/adam_flows/qc_ADVS.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_advs_dataset", type=QCDataArtifact.File(name="qc_advs.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_advs_dataset", type=QCDataArtifact.File(name="qc_advs", type="sas7bdat"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     #PROD
@@ -145,9 +168,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/tfl_flows/t_pop.sas",
         inputs=[Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="t_pop", type=ReportArtifact.File(name="t_pop.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="t_pop", type=ReportArtifact.File(name="t_pop", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     #PROD
@@ -157,9 +181,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         inputs=[Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"]),
                 Input(name="adae_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adae_task["adae_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="t_ae_rel", type=ReportArtifact.File(name="t_ae_rel.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="t_ae_rel", type=ReportArtifact.File(name="t_ae_rel", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     #PROD
@@ -168,9 +193,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="prod/tfl_flows/t_vscat.sas",
         inputs=[Input(name="advs_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=advs_task["advs_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="t_vscat", type=ReportArtifact.File(name="t_vscat.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="t_vscat", type=ReportArtifact.File(name="t_vscat", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     #QC
@@ -179,9 +205,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="qc/tfl_flows/qc_t_pop.sas",
         inputs=[Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="qc_t_pop", type=QCReportArtifact.File(name="qc_t_pop.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_t_pop", type=QCReportArtifact.File(name="qc_t_pop", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     #QC
@@ -191,9 +218,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         inputs=[Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"]),
                 Input(name="qc_adae_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adae_task["qc_adae_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="qc_t_ae_rel", type=QCReportArtifact.File(name="qc_t_ae_rel.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_t_ae_rel", type=QCReportArtifact.File(name="qc_t_ae_rel", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     
     #QC
@@ -202,9 +230,10 @@ def ADaM_TFL_QC(sdtm_dataset_snapshot: str, metadata_snapshot: str):
         command="qc/tfl_flows/qc_t_vscat.sas",
         inputs=[Input(name="qc_advs_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_advs_task["qc_advs_dataset"]),
                 Input(name="metadata_snapshot", type=str, value=metadata_snapshot)],
-        output_specs=[Output(name="qc_t_vscat", type=QCReportArtifact.File(name="qc_t_vscat.pdf"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro"
+        output_specs=[Output(name="qc_t_vscat", type=QCReportArtifact.File(name="qc_t_vscat", type="pdf"))],
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     return 
