ae00b3dfe4acdfdee08ca64ba18c05483f0911b3
Ross Sharp <ross.sharp@dominodatalab.com>
2025-01-22 12:32:58 -0800

    Updating flow_1
    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/CDISC01_ReportingTeam/CDISC01_RE_CSR?executionId=679153e3efde7d519a2cdcce

--- flows/flow_1.py
+++ flows/flow_1.py
@@ -5,13 +5,15 @@ from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
 
-sdtm_dataset="SDTMUNBLIND"
-sdtm_dataset_snapshot_number=1
+
+# Define variables to set the default compute environment and hardware tier for the Flow tasks
+environment_name="SAS Analytics Pro"
+hardware_tier_name="Small"
 
 # Enter the command below to run this Flow. There is a single Flow input parameter for the SDTM Dataset snapshot
-# pyflyte run --remote flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND 
+# pyflyte run --remote ./flows/flow_1.py ADaM_only --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND 
 
-
+# Define one Flow Artifact called ADaM Dataset to tag and group all of the ADAM task outputs as
 DataArtifact = Artifact("ADaM Datasets", DATA)
 
 
@@ -24,11 +26,9 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         command="prod/adam_flows/ADSL.sas",
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
         output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        cache=True,
-        cache_version="1.0"
-       # dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     #Crete ADAE dataset. This has two inputs, the SDTM Dataset and the output from the previous task i.e. ADSL. 
@@ -38,11 +38,9 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
         output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        cache=True,
-        cache_version="1.0"
-        #dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
     
     adcm_task = run_domino_job_task(
@@ -51,11 +49,9 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
         output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        cache=True,
-        cache_version="1.0"
-      #  dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     adlb_task = run_domino_job_task(
@@ -64,11 +60,9 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
         output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        cache=True,
-        cache_version="1.0"
-        #dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     admh_task = run_domino_job_task(
@@ -77,11 +71,9 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
         output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        cache=True,
-        cache_version="1.0"
-      #  dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     advs_task = run_domino_job_task(
@@ -90,11 +82,9 @@ def ADaM_only(sdtm_dataset_snapshot: str):
         inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
                 Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
         output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs.sas7bdat"))],
-        use_project_defaults_for_omitted=True,
-        environment_name="SAS Analytics Pro",
-        cache=True,
-        cache_version="1.0"
-        #dataset_snapshots=[DatasetSnapshot(Name=sdtm_dataset,Version=sdtm_dataset_snapshot_number)]
+        hardware_tier_name=hardware_tier_name,
+        environment_name=environment_name,
+        use_project_defaults_for_omitted=True
     )
 
     return
