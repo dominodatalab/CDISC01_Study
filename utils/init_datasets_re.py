278458892636ccbe28535afd28c29073c25948c0
domino-ross-sharp <132300875+domino-ross-sharp@users.noreply.github.com>
2025-08-12 14:29:04 -0600

    Update init_datasets_re.py

--- utils/init_datasets_re.py
+++ utils/init_datasets_re.py
@@ -110,5 +110,8 @@ for missing_dataset in REQUIRED_MOUNTED.difference(CURRENT_MOUNTED):
 # Create TFL artifacts directory
 os.makedirs("/mnt/artifacts/TFL", exist_ok=True)
 
+# Create QC TFL artifacts directory
+os.makedirs("/mnt/artifacts/TFL_QC", exist_ok=True)
+
 # Create SAS logs artifacts directory
 os.makedirs("/mnt/artifacts/sas_logs", exist_ok=True)
