f431ef082c76b0539a8a02bb1699edc5910e1486
domino-ross-sharp <132300875+domino-ross-sharp@users.noreply.github.com>
2025-08-12 15:16:43 -0600

    Update init_datasets_re.py

--- utils/init_datasets_re.py
+++ utils/init_datasets_re.py
@@ -13,6 +13,7 @@ domino = Domino(f"{DOMINO_PROJECT_OWNER}/{DOMINO_PROJECT_NAME}")
 
 # Required Datasets & Descriptions
 REQUIRED = {
+    "METADATA": "Internal metadata",
     "COMPARE": "PROC COMPARE datasets for QC",
     "ADAM": "ADAM is created using SDTM data for production",
     "ADAMQC": "ADAMQC is created using SDTM data for qc"
