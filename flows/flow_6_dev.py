e0e86fa7e48499a1b367b42ae6842e791c429f97
Jim Coates <jim.coates@dominodatalab.com>
2025-10-29 08:27:22 -0700

    Copied to flow 6 new
    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/CDISC01_ReportingTeam/CDISC01_RE_CSR?executionId=69021c794f6c7606cf664201

--- flows/flow_6_dev.py
+++ flows/flow_6_dev.py
@@ -14,7 +14,7 @@ hardware_tier_name="Small"
 cache = False
 
 # Enter the command below to run this Flow. There is a single Flow input parameter for the SDTM Dataset snapshot
-# pyflyte run --remote ./flows/flow_5_dev.py SDTM_ADaM_TFL --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
+# pyflyte run --remote ./flows/flow_6_dev.py SDTM_ADaM_TFL --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
 
 # If you want to give the run a name, then use this command and replace the MY_CUSTOM_NAME argument
 # pyflyte run --remote --name ENTER_RUN_NAME ./flows/flow_5_dev.py SDTM_ADaM_TFL --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND --metadata_snapshot /mnt/data/METADATA 
@@ -249,7 +249,7 @@ def SDTM_ADaM_TFL(sdtm_dataset_snapshot: str, metadata_snapshot: str):
     # Pinnacle21 Validation - Validate all ADaM datasets against CDISC standards
     p21_validation_task = run_domino_job_task(
         flyte_task_name="Pinnacle21 CDISC Validation",
-        command="prod/validation/p21_validation.py",
+        command="code/prod/validation/p21_validation.py",
         inputs=[
             Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"]),
             Input(name="adae_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adae_task["adae_dataset"]),
