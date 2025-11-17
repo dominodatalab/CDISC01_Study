12bfa91c690de93ea84d7257248833116c2d6cb8
domino-ross-sharp <132300875+domino-ross-sharp@users.noreply.github.com>
2025-11-17 18:13:11 +0100

    Update flow_3_dev.py

--- flows/flow_3_dev.py
+++ flows/flow_3_dev.py
@@ -2,39 +2,42 @@ from flytekit import workflow
 from flytekit.types.file import FlyteFile
 from typing import TypeVar, NamedTuple, Tuple
 from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
-from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
+from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot, NetAppVolumeSnapshot
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
 
 
 # Define variables to set the default compute environment and hardware tier for the Flow tasks
 environment_name="SAS Analytics Pro"
 hardware_tier_name="Small"
+GitRef_type="branches"                                     
+GitRef_value="prod_netapp"  
 
 # Default for caching, set to True or False
-cache = False
+cache = True
 
 # Enter the command below to run this Flow. There is a single Flow input parameter for the SDTM Dataset snapshot
-# pyflyte run --remote ./flows/flow_3_dev.py ADaM_only_QC --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND
+# pyflyte run --remote ./flows/flow_3_dev.py ADaM_only_QC --netapp_volume_snapshot /mnt/netapp-volumes/CDISC01_SDTMBLIND
 
 # If you want to give the run a name, then use this command and replace the MY_CUSTOM_NAME argument
-# pyflyte run --remote --name ENTER_RUN_NAME ./flows/flow_3_dev.py ADaM_only_QC --sdtm_dataset_snapshot /mnt/imported/data/SDTMBLIND
-
+# pyflyte run --remote --name ENTER_RUN_NAME ./flows/flow_3_dev.py ADaM_only_QC ---netapp_volume_snapshot /mnt/netapp-volumes/CDISC01_SDTMBLIND
 
 # Define two Flow Artifacts called ADaM Dataset and QC ADaM Dataset to tag and group ADaM outputs respectively
 DataArtifact = Artifact("ADaM Datasets", DATA)
 QCDataArtifact = Artifact("QC ADaM Datasets", DATA)
 
 @workflow
-def ADaM_only_QC(sdtm_dataset_snapshot: str):
+def ADaM_only_QC(netapp_volume_snapshot: str):
 
     #PROD 
     adsl_task = run_domino_job_task(
         flyte_task_name="Create ADSL Dataset",
         command="prod/adam/ADSL.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="adsl_dataset", type=DataArtifact.File(name="adsl", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot)],
+        output_specs=[Output(name="adsl", type=DataArtifact.File(name="adsl", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -44,11 +47,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     adae_task = run_domino_job_task(
         flyte_task_name="Create ADAE Dataset",
         command="prod/adam/ADAE.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adae_dataset", type=DataArtifact.File(name="adae", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="adsl", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl"])],
+        output_specs=[Output(name="adae", type=DataArtifact.File(name="adae", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -57,11 +62,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     adcm_task = run_domino_job_task(
         flyte_task_name="Create ADCM Dataset",
         command="prod/adam/ADCM.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adcm_dataset", type=DataArtifact.File(name="adcm", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="adsl", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl"])],
+        output_specs=[Output(name="adcm", type=DataArtifact.File(name="adcm", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -70,11 +77,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     adlb_task = run_domino_job_task(
         flyte_task_name="Create ADLB Dataset",
         command="prod/adam/ADLB.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="adlb_dataset", type=DataArtifact.File(name="adlb", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="adsl", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl"])],
+        output_specs=[Output(name="adlb", type=DataArtifact.File(name="adlb", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0",
@@ -83,11 +92,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     admh_task = run_domino_job_task(
         flyte_task_name="Create ADMH Dataset",
         command="prod/adam/ADMH.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="admh_dataset", type=DataArtifact.File(name="admh", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="adsl", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl"])],
+        output_specs=[Output(name="admh", type=DataArtifact.File(name="admh", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -96,11 +107,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     advs_task = run_domino_job_task(
         flyte_task_name="Create ADVS Dataset",
         command="prod/adam/ADVS.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl_dataset"])],
-        output_specs=[Output(name="advs_dataset", type=DataArtifact.File(name="advs", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="adsl", type=FlyteFile[TypeVar("sas7bdat")], value=adsl_task["adsl"])],
+        output_specs=[Output(name="advs", type=DataArtifact.File(name="advs", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -109,10 +122,12 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     qc_adsl_task = run_domino_job_task(
         flyte_task_name="Create QC ADSL Dataset",
         command="qc/adam/qc_ADSL.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot)],
-        output_specs=[Output(name="qc_adsl_dataset", type=QCDataArtifact.File(name="qc_adsl", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot)],
+        output_specs=[Output(name="qc_adsl", type=QCDataArtifact.File(name="qc_adsl", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0",
@@ -122,11 +137,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     qc_adae_task = run_domino_job_task(
         flyte_task_name="Create QC ADAE Dataset",
         command="qc/adam/qc_ADAE.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_adae_dataset", type=QCDataArtifact.File(name="qc_adae", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="qc_adsl", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl"])],
+        output_specs=[Output(name="qc_adae", type=QCDataArtifact.File(name="qc_adae", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -135,11 +152,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     qc_adcm_task = run_domino_job_task(
         flyte_task_name="Create QC ADCM Dataset",
         command="qc/adam/qc_ADCM.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_adcm_dataset", type=QCDataArtifact.File(name="qc_adcm", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="qc_adsl", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl"])],
+        output_specs=[Output(name="qc_adcm", type=QCDataArtifact.File(name="qc_adcm", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -148,11 +167,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     qc_adlb_task = run_domino_job_task(
         flyte_task_name="Create QC ADLB Dataset",
         command="qc/adam/qc_ADLB.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_adlb_dataset", type=QCDataArtifact.File(name="qc_adlb", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="qc_adsl", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl"])],
+        output_specs=[Output(name="qc_adlb", type=QCDataArtifact.File(name="qc_adlb", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -161,11 +182,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     qc_admh_task = run_domino_job_task(
         flyte_task_name="Create QC ADMH Dataset",
         command="qc/adam/qc_ADMH.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_admh_dataset", type=QCDataArtifact.File(name="qc_admh", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="qc_adsl", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl"])],
+        output_specs=[Output(name="qc_admh", type=QCDataArtifact.File(name="qc_admh", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
@@ -174,11 +197,13 @@ def ADaM_only_QC(sdtm_dataset_snapshot: str):
     qc_advs_task = run_domino_job_task(
         flyte_task_name="Create QC ADVS Dataset",
         command="qc/adam/qc_ADVS.sas",
-        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=sdtm_dataset_snapshot),
-                Input(name="qc_adsl_dataset", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl_dataset"])],
-        output_specs=[Output(name="qc_advs_dataset", type=QCDataArtifact.File(name="qc_advs", type="sas7bdat"))],
+        inputs=[Input(name="sdtm_snapshot_task_input", type=str, value=netapp_volume_snapshot),
+                Input(name="qc_adsl", type=FlyteFile[TypeVar("sas7bdat")], value=qc_adsl_task["qc_adsl"])],
+        output_specs=[Output(name="qc_advs", type=QCDataArtifact.File(name="qc_advs", type="sas7bdat"))],
         hardware_tier_name=hardware_tier_name,
         environment_name=environment_name,
+        netapp_volume_snapshots=[NetAppVolumeSnapshot(Id="c4b37e73-55bc-4f75-84af-7cd2ea9046ba", Version=1)],
+        main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         use_project_defaults_for_omitted=True,
         cache=cache,
         cache_version="1.0"
